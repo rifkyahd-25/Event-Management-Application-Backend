f4705f77fcff7fd01c7379a42feb907298076882
Rifky <s22010240@ousl.lk>
2025-02-11 20:50:56 +0530

    SignUp Api Finished

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,35 @@
-import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js';
-
-// export const signup = async (req, res) => {
-// res.json({ message: 'Signup route' });
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from 'bcryptjs';
 
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required!"));
+  }
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const newuser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-// }
+  try {
+    await newuser.save();
+    res.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
 
-
-export const test = (req, res) => {
-    res.json({ message: 'API is working!' });
-  };
-  
+// export const test = (req, res) => {
+//     res.json({ message: 'API is working!' });
+//   };
--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import mangoose from "mongoose";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import cors from 'cors';
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,8 +20,7 @@ mongoose
     console.log(err);
   });
 
-const app = express();
-app.use(express.json());
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");

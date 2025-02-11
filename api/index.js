677ef56d96d460d72b2e2d9f3bb3692ec2f7aaf7
Rifky <s22010240@ousl.lk>
2025-02-11 20:06:28 +0530

    Add server and Database

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mangoose from "mongoose";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});

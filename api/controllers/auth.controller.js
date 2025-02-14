7a4da7cac104717d8910c12c0da3e88a91a8ee43
Rifky <s22010240@ousl.lk>
2025-02-14 18:50:40 +0530

    SignIn functionality Finished with jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
-import bcrypt from 'bcryptjs';
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -30,6 +30,32 @@ export const signup = async (req, res, next) => {
   }
 };
 
-// export const test = (req, res) => {
-//     res.json({ message: 'API is working!' });
-//   };
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required!"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(400, "Invalid credentials"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(400, "Invalid credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc; //removing password from user object
+
+    res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+      })//sending token in cookie
+      .json(rest);//sending user object without password
+  } catch (error) {
+    next(error);
+  }
+};
